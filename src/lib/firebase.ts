43f3f27f8ba511b927d6dfecda88a01a0be76817
Refer and Earn <shadilrayyan1@gmail.com>
2025-08-24 13:48:36 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+"use client"
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "API_KEY",
+  authDomain: "devfolio-be2e3.firebaseapp.com",
+  projectId: "devfolio-be2e3",
+  storageBucket: "devfolio-be2e3.appspot.com",
+  messagingSenderId: "951339498218",
+  appId: "1:951339498218:web:736c9a3b648376435c2b64",
 };
 
 // Initialize Firebase
